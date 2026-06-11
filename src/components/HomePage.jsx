9357b576c12e093351641fe345ec0ded55c672ba
Hujefa Pawaskar <pawaskarhujefa5@gmail.com>
2026-06-11 12:38:44 +0530

    logo fixed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,9 +12,9 @@ const HomePage = () => {
         backgroundPosition: `center calc(100% + 40px)` // Adjust the 50px or 0% (top) / 100% (bottom) to your exact needs
       }}
     >
-      <div className="p-5">
+      <div className="p-3 ml-15">
         <NuthandasLogoAnimated 
-          className="w-48 md:w-64 h-70 transition-all duration-500 hover:scale-105 hover:drop-shadow-[0_0_15px_rgba(218,165,32,0.7)] cursor-pointer"
+          className="w-48 md:w-74 h-70 transition-all duration-500 hover:scale-105 hover:drop-shadow-[0_0_15px_rgba(218,165,32,0.7)] cursor-pointer"
         />
       </div>
 
