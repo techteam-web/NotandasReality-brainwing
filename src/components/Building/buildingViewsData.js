d580595926a9438b924b822f45fd54ff9a44ecc3
Hujefa Pawaskar <pawaskarhujefa5@gmail.com>
2026-07-04 17:03:52 +0530

    respnosvie added bp

--- src/components/Building/buildingViewsData.js
+++ src/components/Building/buildingViewsData.js
@@ -28,7 +28,8 @@ export const BUILDING_VIEWS = {
     // every floor SVG shares this coordinate space
     viewBox: "0 0 1672 941",
     floors: NOTAN_DC_FLOORS,
-    headerClass: "left-80 top-36 md:top-127 2xl:left-64 2xl:top-90 xl:left-20 xl:top-90 lg:left-20 lg:top-70 3xl:left-90 3xl:top-105 4xl:left-137 4xl:top-150",
+    headerClass: "left-80 top-36 md:top-127 2xl:left-66 2xl:top-107 xl:left-45  xl:top-90 lg:left-20 lg:top-70 3xl:left-95 3xl:top-105 4xl:left-137 4xl:top-150",
+    amenityClass: "bottom-55 left-75 md:bottom-15 md:left-15 lg:bottom-15 lg:left-15 lg:h-35 2xl:h-65 2xl:left-57 2xl:top-137 3xl:top-150 3xl:left-87 3xl:h-75 4xl:top-190 4xl:left-148 4xl:h-76 xl:h-62 xl:left-32 xl:top-115 ",
   
   },
   "notan-edge": {
@@ -53,8 +54,8 @@ export const BUILDING_VIEWS = {
     // matches the combined Building_Floor_SVG/Nothan_space/Notan_space_.svg
     viewBox: "0 0 1672 941",
     floors: NOTAN_SPACE_FLOORS,
-   amenityClass: "bottom-55 left-75 md:bottom-15 md:left-15 lg:bottom-15 lg:left-15 lg:h-35  3xl:top-165 3xl:left-87 4xl:bottom-70 4xl:left-90 xl:h-52 xl:left-27 ",
-    headerClass: "left-80 top-36 md:top-127 2xl:left-24 2xl:top-90 xl:left-23 xl:top-74 lg:left-23 lg:top-54 3xl:left-80 3xl:top-109 4xl:left-90 4xl:top-150",
+   amenityClass: "bottom-55 left-75 md:bottom-15 md:left-15 lg:bottom-15 lg:left-15 lg:h-35 2xl:h-57 2xl:left-55 3xl:top-165 3xl:left-87 4xl:bottom-70 4xl:left-90 xl:h-52 xl:left-27 ",
+    headerClass: "left-80 top-36 md:top-127 2xl:left-47 2xl:top-114  xl:left-23 xl:top-74  lg:left-23 lg:top-54 3xl:left-80 3xl:top-109 4xl:left-90 4xl:top-150",
   
    
   },
