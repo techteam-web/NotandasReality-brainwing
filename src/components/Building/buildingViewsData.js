8a055c1c880fe3c718d725bd66605fdd4bed2bcf
Hujefa Pawaskar <pawaskarhujefa5@gmail.com>
2026-07-08 14:06:05 +0530

    respnosive added building page

--- src/components/Building/buildingViewsData.js
+++ src/components/Building/buildingViewsData.js
@@ -28,7 +28,7 @@ export const BUILDING_VIEWS = {
     // every floor SVG shares this coordinate space
     viewBox: "0 0 1672 941",
     floors: NOTAN_DC_FLOORS,
-    headerClass: "left-80 top-36 md:top-127 2xl:left-66 2xl:top-107 xl:left-45  xl:top-90 lg:left-20 lg:top-70 3xl:left-95 3xl:top-105 4xl:left-137 4xl:top-150",
+    headerClass: "left-80 top-36 md:top-127 2xl:left-74 2xl:top-107 xl:left-55  xl:top-90 lg:left-20 lg:top-70 3xl:left-102 3xl:top-105 4xl:left-137 4xl:top-150",
     amenityClass: "bottom-55 left-75 md:bottom-15 md:left-15 lg:bottom-15 lg:left-15 lg:h-35 2xl:h-65 2xl:left-57 2xl:top-137 3xl:top-150 3xl:left-87 3xl:h-75 4xl:top-190 4xl:left-148 4xl:h-76 xl:h-62 xl:left-32 xl:top-115 ",
   
   },
@@ -44,8 +44,8 @@ export const BUILDING_VIEWS = {
     // matches the combined Building_Floor_SVG/Nothan_jewel/nothan_jewel-2.8x.svg
     viewBox: "0 0 4615 2597",
     floors: NOTAN_JEWEL_FLOORS,
-    amenityClass: "bottom-55 left-75 md:bottom-15 md:left-15 lg:bottom-15 lg:left-15 lg:h-35  3xl:top-165 3xl:left-87 4xl:bottom-70 4xl:left-90 xl:h-52 xl:left-27 ",
-    headerClass: "left-80 top-36 md:top-127 2xl:left-24 2xl:top-90 xl:left-23 xl:top-74 lg:left-23 lg:top-54 3xl:left-70 3xl:top-127 4xl:left-90 4xl:top-150",
+    amenityClass: "bottom-55 left-75 md:bottom-15 md:left-15 lg:bottom-15 lg:left-15 lg:h-35 2xl:left-55 2xl:top-133 2xl:h-60 3xl:top-165 3xl:left-87 4xl:bottom-70 4xl:left-90 xl:h-52 xl:left-27 ",
+    headerClass: "left-80 top-36 md:top-127 2xl:left-59 2xl:top-90 xl:left-38 xl:top-74 lg:left-23 lg:top-54 3xl:left-89 3xl:top-127 4xl:left-90 4xl:top-150",
   
   // default header/aside positions are fine for this one, so no need to override
   },
@@ -72,7 +72,7 @@ export const BUILDING_VIEWS = {
     // matches the combined Building_Floor_SVG/Nothan_crown/Notan_crown_.svg
     viewBox: "0 0 1672 941",
     floors: NOTAN_CROWN_FLOORS,
-    headerClass: "left-80 top-36 md:top-127 2xl:left-64 2xl:top-90 xl:left-20 xl:top-90 lg:left-20 lg:top-70 3xl:left-80 3xl:top-127 4xl:left-137 4xl:top-150",
+    headerClass: "left-80 top-36 md:top-127 2xl:left-74 2xl:top-105 xl:left-60 xl:top-100 lg:left-20 lg:top-70 3xl:left-97 3xl:top-127 4xl:left-137 4xl:top-150",
       asideClass: "left-[25%] top-[20%] md:right-12 md:w-44 2xl:top-[10%] xl:top-[10%]  lg:top-[5%] 3xl:top-[14%] 4xl:top-[10%]",
   },
 };
