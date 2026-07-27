2b14ff66002add5e7c522e931980394e03fd1928
Hujefa Pawaskar <pawaskarhujefa5@gmail.com>
2026-07-27 17:55:53 +0530

    respon added

--- src/components/Building/buildingViewsData.js
+++ src/components/Building/buildingViewsData.js
@@ -49,12 +49,14 @@ export const BUILDING_VIEWS = {
     viewBox: "-9554435 -6002850 4615 2597",
     floors: NOTAN_EDGE_FLOORS,
     headerClass:
-      "left-80 top-36 md:top-127 2xl:left-74 2xl:top-90 xl:left-55  xl:top-65 lg:left-30 lg:top-50 3xl:left-102 3xl:top-105 4xl:left-137 4xl:top-150",
+      "left-80 top-36 md:top-127 2xl:left-74 2xl:top-60 xl:left-55  xl:top-65 lg:left-45 lg:top-15 3xl:left-102 3xl:top-70 4xl:left-137 4xl:top-110",
     headerLogoClass:
-      "my-[-30%] w-64 sm:w-72 md:w-100 lg:w-76 xl:w-68 2xl:w-76 4xl:w-130",
+      "my-[-30%] w-64 sm:w-72 md:w-100 lg:w-36 xl:w-68 2xl:w-76 4xl:w-130",
     nMarkFill: "black",
     amenityClass:
       "w-[calc(100%-2.5rem)] max-w-xl sm:max-w-2xl lg:max-w-3xl bottom-55 left-75 md:bottom-15 md:left-15 lg:bottom-15 lg:left-15 2xl:left-55 3xl:left-39 4xl:left-120 xl:left-27",
+      headerSubClass:
+      "lg:-mt-14 lg:text-[7px] xl:text-[-23px]  xl:mt-8 2xl:mt-3 2xl:text-[8px] 2xl:text-bold 3xl:mt-5 3xl:text-[10px] 4xl:mt-7 4xl:text-[16px]",
     amenityListClass: "max-w-lg",
     amenityItemClass:
       "lg:text-[16px] xl:text-[15.5px] 2xl:text-[15.2px] 3xl:text-[18px] 4xl:text-[25px]",
@@ -105,15 +107,19 @@ export const BUILDING_VIEWS = {
     viewBox: "0 0 4615 2597",
     floors: NOTAN_TERRACE_FLOORS,
     headerClass:
-      "left-80 top-36 md:top-127 2xl:left-60 2xl:top-70 xl:left-45  xl:top-55 lg:left-17 lg:top-40 3xl:left-90 3xl:top-105 4xl:left-137 4xl:top-150 ",
+      "left-80 top-36 md:top-127 2xl:left-70 2xl:top-60 xl:left-45  xl:top-55 lg:left-37 lg:top-25 3xl:left-90 3xl:top-70 4xl:left-120 4xl:top-100 ",
     headerLogoClass:
-      "my-[-30%] w-64 sm:w-72 md:w-100 lg:w-76 xl:w-68 2xl:w-76 4xl:w-130",
+      "my-[-30%] w-64 sm:w-72 md:w-100 lg:w-46 xl:w-68 2xl:w-76 4xl:w-130",
+       headerSubClass:
+      "lg:-mt-12 lg:text-[8px] xl:text-[-23px]  xl:mt-8 2xl:mt-3 2xl:text-[10px] 2xl:text-bold 3xl:mt-5 3xl:text-[10px] 4xl:mt-7 4xl:text-[16px]",
     nMarkFill: "black",
     amenityClass:
       "w-[calc(100%-2.5rem)] max-w-xl sm:max-w-2xl lg:max-w-3xl bottom-55 left-75 md:bottom-15 md:left-15 lg:bottom-15 lg:left-15 2xl:left-55 3xl:left-39 4xl:left-120 xl:left-27",
     amenityListClass: "max-w-lg",
     amenityItemClass:
       "lg:text-[16px] xl:text-[15.5px] 2xl:text-[15.2px] 3xl:text-[18px] 4xl:text-[25px]",
+      asideClass:
+      "left-[65%] top-[30%] xl:top-[24%] 2xl:left-[65%] 2xl:top-[30%] xl:top-[10%]  lg:top-[15%] 3xl:top-[18%] 4xl:top-[25%]",
   },
   "notan-crown": {
     viewImg: notanCrownView,
@@ -121,8 +127,10 @@ export const BUILDING_VIEWS = {
     viewBox: "0 0 1672 941",
     floors: NOTAN_CROWN_FLOORS,
     headerClass:
-      "left-80 top-36 md:top-127 2xl:left-90 2xl:top-105 xl:left-79 xl:top-100 lg:left-40 lg:top-90 3xl:left-108 3xl:top-127 4xl:left-137 4xl:top-150",
-    headerLogoClass: "w-44 sm:w-52 md:w-72 lg:w-56 xl:w-48 2xl:w-56 2xl:h-20 3xl:h-26 4xl:w-96",
+      "left-80 top-36 md:top-127 2xl:left-79 2xl:top-105 xl:left-59 xl:top-90 lg:left-45 lg:top-65 3xl:left-108 3xl:top-127 4xl:left-137 4xl:top-150",
+      headerSubClass:
+      "lg:mt-3 lg:text-[8px] ",
+    headerLogoClass: "w-34  lg:w-36 lg:h-[4.2rem] xl:w-47 xl:h-[5.25rem] 2xl:w-56 2xl:h-[6.25rem] 3xl:h-36 3xl:w-59 4xl:w-76 4xl:h-[10.5rem]",
     asideClass:
       "left-[25%] top-[20%] md:right-12 md:w-44 2xl:top-[10%] xl:top-[10%]  lg:top-[5%] 3xl:top-[14%] 4xl:top-[10%]",
     nMarkFill: "black",
