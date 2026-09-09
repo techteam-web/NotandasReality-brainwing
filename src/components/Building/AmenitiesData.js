8fcdfb8ac2cf1c8f9babdee9b07999a2851de589
Hujefa Pawaskar <pawaskarhujefa5@gmail.com>
2026-09-09 12:52:52 +0530

    pipes added amenities

--- src/components/Building/AmenitiesData.js
+++ src/components/Building/AmenitiesData.js
@@ -6,14 +6,14 @@ export const BUILDING_AMINITIES = {
   },
 
   "notan-space": {
-    1: "Grand Lobby with Reception and Lounge | Ground-Floor Café and Lounge|",
+    1: "Grand Lobby with Reception and Lounge | Ground-Floor Café and Lounge |",
     2: "Private Pantry and Washroom in Every Unit |",
     3: "3 High-Speed Elevators | Separate Service/Fire Elevator",
   },
 
   "notan-jewel": {
-    1: "Signature Lobby Lounge | Dedicated Reception Desks|",
-    2: "Intelligent Car Tower Parking | 3 High-Speed Elevators|",
+    1: "Signature Lobby Lounge | Dedicated Reception Desks |",
+    2: "Intelligent Car Tower Parking | 3 High-Speed Elevators |",
     3: "Refuge Zones & Double-Height Deck",
   },
 
@@ -31,9 +31,9 @@ export const BUILDING_AMINITIES = {
   },
 
   "notan-views": {
-    1: "Grand Arrival Lobby | Multi-Purpose Hall | Banquet Lounge | Secure Podium-Level Parking|",
-    2: "24-Hour Security & Surveillance | Hospitality & Concierge Services | High-Speed Elevators & Service Lift|",
-    3: "State-of-the-Art Fitness Centre | Landscaped Green Pockets with Water Features|",
+    1: "Grand Arrival Lobby | Multi-Purpose Hall | Banquet Lounge | Secure Podium-Level Parking |",
+    2: "24-Hour Security & Surveillance | Hospitality & Concierge Services | High-Speed Elevators & Service Lift |",
+    3: "State-of-the-Art Fitness Centre | Landscaped Green Pockets with Water Features |",
     4: "Outdoor Leisure Decks & Gazebos | Dedicated Staff Facilities | Open-to-Sky Rooftop Walking Track",
   },
 
@@ -45,16 +45,16 @@ export const BUILDING_AMINITIES = {
   },
 
   "notan-beach-house": {
-    1: "Grand Double-Height Lobby & Reception | Landscaped Garden with Beach Access | Open to Sky Pool|",
-    2: "Rooftop Leisure Decks | Salon & Wellness Services | Fully Equipped Fitness Centre|",
-    3: "24-Hour Security & Surveillance | Hospitality & Concierge Services | Car Lift with Basement Parking|",
+    1: "Grand Double-Height Lobby & Reception | Landscaped Garden with Beach Access | Open to Sky Pool |",
+    2: "Rooftop Leisure Decks | Salon & Wellness Services | Fully Equipped Fitness Centre |",
+    3: "24-Hour Security & Surveillance | Hospitality & Concierge Services | Car Lift with Basement Parking |",
     4: "High-Speed Passenger Elevators & Dedicated Service Lift | Dedicated Staff Facilities",
   },
 
   "notan-tides": {
-    1: "Owner’s Lounge | Rooftop 25-Meter Half-Olympic Infinity Pool with Panoramic Views",
-    2: "Jogging Track Encircling the Skyline | Bespoke Fitness Centre and Wellness Deck",
-    3: "Private Concierge & Valet Services | Grand Double-Height Lobby | 2-Way Ramp Parking Access",
+    1: "Owner’s Lounge | Rooftop 25-Meter Half-Olympic Infinity Pool with Panoramic Views |",
+    2: "Jogging Track Encircling the Skyline | Bespoke Fitness Centre and Wellness Deck |",
+    3: "Private Concierge & Valet Services | Grand Double-Height Lobby | 2-Way Ramp Parking Access |",
     4: "Sea-Facing Sit-Outs & Landscaped Terraces",
   },
 
